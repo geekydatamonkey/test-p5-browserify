848b393d12b4a8d0146fb1d9c36d5798ae4a67f6
James Johnson <johnsojr@gmail.com>
2015-07-22 18:00:27 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var p5 = require('p5');
+
+// function sketch(s) {
+//   s.setup = function() {
+//     s.createCanvas(400,400);
+//     s.fill(100);
+//     s.ellipse(100,100,10,10);
+//   }
+// }
+
+// new p5(sketch);
